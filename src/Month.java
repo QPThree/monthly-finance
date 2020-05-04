ed90346a963d76859b793eebca994a07d5d0a16c
QPThree <youngqp3@gmail.com>
2020-05-03 18:43:45 -0700

    Initial Commit

---
+++ src/Month.java
@@ -0,0 +1,52 @@
+
+public class Month {
+	
+	//income
+	double basepay;
+	int bah;
+	int bas;
+	//expenses
+	int rent;
+	int creditcard;
+	
+	//getters & setters
+	public double getbasepay() {
+		return basepay;
+	}
+	
+	public void setbasepay(int b) {
+		this.basepay = b - (b * .12);
+	}
+	public int getbah() {
+		return bah;
+	}
+	
+	public void setbah(int b) {
+		this.bah = b;
+	}
+	public int getbas() {
+		return bas;
+	}
+	
+	public void setbas(int b) {
+		this.bas = b;
+	}
+	public int getrent() {
+		return rent;
+	}
+	public void setrent(int b) {
+		this.rent = b;
+	}
+	public int getcreditcard() {
+		return creditcard;
+	}
+	public void setcreditcard(int b) {
+		this.creditcard = b;
+	}
+	
+	public void leftover(double x, int y) {
+		double z = x - y;
+		System.out.println("You have "+z+" remaining to invest");
+		}
+	
+}
---
+++ src/MyFinance.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class MyFinance {
+
+	public static void main(String[] args) {
+		//create scanner
+		Scanner scanner = new Scanner (System.in);
+		
+		//create  month for financial analysis
+		
+		Month month = new Month();
+		
+		//prompts user to enter payment amounts
+		System.out.println("Enter your bah amount");
+		month.setbah(scanner.nextInt());
+		System.out.println("Enter your bas amount");
+		month.setbas(scanner.nextInt());
+		System.out.println("Enter your base pay amount");
+		month.setbasepay(scanner.nextInt());
+		
+		//prompts user for their expenses
+		System.out.println("How much did you pay on rent this much?");
+		month.setrent(scanner.nextInt());
+		System.out.println("How much was your credit card payment?");
+		month.setcreditcard(scanner.nextInt());
+		
+		//prompts user for QQQ's share price
+		System.out.println("What is QQQ's stock price at?");
+		double qqq = scanner.nextDouble();
+		
+		//make variables for total income and expense
+		int out = month.getrent() + month.getcreditcard();
+		double in = month.getbah() +  month.getbas() + month.getbasepay();
+		double left = in - out;
+		
+		
+		
+		//prints out the entered amounts
+		System.out.println("IN: ");
+		System.out.println(in);
+		System.out.println("OUT: ");
+		System.out.println(out);
+		month.leftover(in, out);
+		shares (left, qqq);
+		
+		
+		
+		
+	}
+	//methods 2
+	public static void shares (double leftover, double qqq) {
+		double z =  leftover / qqq;
+		System.out.printf("You can buy %.0f shares of QQQ", z);
+	}
+	
+}
+
+
